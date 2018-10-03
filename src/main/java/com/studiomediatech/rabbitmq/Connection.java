c2402a62de85ea6f3f82dbf187e563825e245d93
Olle Törnström <toernstroem@synyx.de>
2018-10-03 22:42:11 +0200

    Stupid simple frame decoder

--- src/main/java/com/studiomediatech/rabbitmq/Connection.java
+++ src/main/java/com/studiomediatech/rabbitmq/Connection.java
@@ -2,19 +2,23 @@ package com.studiomediatech.rabbitmq;
 
 import io.netty.bootstrap.Bootstrap;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
+import io.netty.handler.codec.ReplayingDecoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+
+import java.util.List;
 
 
 /**
@@ -38,6 +42,7 @@ public final class Connection {
                         public void initChannel(SocketChannel ch) throws Exception {
 
                             ch.pipeline()
+                            .addLast("decoder", new AmqpFrame.Decoder())
                             .addLast("logger", new LoggingHandler(LogLevel.INFO))
                             .addLast("handler", new Negotiate());
                         }
@@ -49,12 +54,62 @@ public final class Connection {
         }
     }
 
-    static class Negotiate extends ChannelInboundHandlerAdapter {
+    static class Negotiate extends SimpleChannelInboundHandler<AmqpFrame> {
 
         @Override
         public void channelActive(ChannelHandlerContext ctx) throws Exception {
 
             ctx.writeAndFlush(Unpooled.copiedBuffer(new byte[] { 'A', 'M', 'Q', 'P', 0, 0, 9, 1 }));
+        }
+
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, AmqpFrame msg) throws Exception {
+
+            System.out.println(">>>>> RECEIVED FRAME: " + msg);
+        }
+    }
+
+    static class AmqpFrame {
+
+        private final int type;
+        private final int channel;
+        private final int size;
+        private byte[] payload;
+
+        private AmqpFrame(int type, int channel, int size) {
+
+            this.type = type;
+            this.channel = channel;
+            this.size = size;
+        }
+
+        @Override
+        public String toString() {
+
+            return "AmqpFrame [type=" + type + ", channel=" + channel + ", size=" + size + ", payload-bytes-read="
+                + payload.length + "]";
+        }
+
+        static class Decoder extends ReplayingDecoder<AmqpFrame> {
+
+            @Override
+            protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+
+                int type = (int) in.readByte();
+                int channel = in.readShort();
+                int size = in.readInt();
+
+                byte[] payload = new byte[size];
+                in.readBytes(payload);
+
+                assert 0xce == in.readByte();
+
+                AmqpFrame frame = new AmqpFrame(type, channel, size);
+                frame.payload = payload;
+
+                out.add(frame);
+            }
         }
     }
 }
